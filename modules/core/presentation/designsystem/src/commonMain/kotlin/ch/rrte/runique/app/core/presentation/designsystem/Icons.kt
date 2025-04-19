3e492b4805c8225c36e1667632eef76afbb04986
Roland Ringgenberg <roland@rolandringgenberg.com>
2025-04-19 15:49:25 +0200

    Added the icons

---
+++ modules/core/presentation/designsystem/src/commonMain/kotlin/ch/rrte/runique/app/core/presentation/designsystem/Icons.kt
@@ -0,0 +1,121 @@
+package ch.rrte.runique.app.core.presentation.designsystem
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import org.jetbrains.compose.resources.vectorResource
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.Res
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.analytics
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.arrow_left
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.arrow_right
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.calendar
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.check
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.cross
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.danger
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.email
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.eye_closed
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.eye_opened
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.finish
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.keyboard_arrow_down
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.keyboard_arrow_up
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.location
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.lock
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.logo
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.logout
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.pause
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.person
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.run_now
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.run_now_outlined
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.start
+import rrtech_pg_pl_runique_app.modules.core.presentation.designsystem.generated.resources.stop
+
+val AnalyticsIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.analytics)
+
+val ArrowLeftIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.arrow_left)
+
+val ArrowRightIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.arrow_right)
+
+val CalendarIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.calendar)
+
+val CheckIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.check)
+
+val CrossIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.cross)
+
+val EmailIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.email)
+
+val ExclamationMarkIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.danger)
+
+val EyeClosedIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.eye_closed)
+
+val EyeOpenedIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.eye_opened)
+
+val FinishIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.finish)
+
+val KeyboardArrowDownIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.keyboard_arrow_down)
+
+val KeyboardArrowUpIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.keyboard_arrow_up)
+
+val LocationIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.location)
+
+val LockIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.lock)
+
+val LogoIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.logo)
+
+val LogoutIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.logout)
+
+val PauseIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.pause)
+
+val PersonIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.person)
+
+val RunOutlinedIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.run_now_outlined)
+
+val RunIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.run_now)
+
+val StartIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.start)
+
+val StopIcon: ImageVector
+    @Composable
+    get() = vectorResource(resource = Res.drawable.stop)
